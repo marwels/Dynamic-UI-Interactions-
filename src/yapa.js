2763e33d4dce768a4a88fa30815f2bbc3103e0d0
marwels <mwelfe@gmail.com>
2023-03-08 15:05:48 +0100

    yapa: separate shadows

--- src/yapa.js
+++ src/yapa.js
@@ -1,14 +1,21 @@
 const LowerThird = function Nav(parentEl) {
   const LowerThirdContainer = document.createElement("div");
   LowerThirdContainer.className = "LowerThirdContainer";
+
+  const firstRow2 = document.createElement("div");
+  firstRow2.innerText = "EKT Gdynia";
+  firstRow2.className = "firstRow shadow";
+  LowerThirdContainer.appendChild(firstRow2);
+
   const firstRow = document.createElement("div");
   firstRow.innerText = "EKT Gdynia";
-  firstRow.className = "firstRow";
+  firstRow.className = "firstRow jump";
   LowerThirdContainer.appendChild(firstRow);
+
   const secondRow = document.createElement("div");
   secondRow.className = "secondRow";
   const secondRowText = document.createElement("div");
-  secondRowText.innerText = "dobrze jest";
+  secondRowText.innerText = "dobrze jest nie być głodnym";
   secondRowText.className = "secondRowText";
   secondRow.appendChild(secondRowText);
   const triangle = document.createElement("div");
