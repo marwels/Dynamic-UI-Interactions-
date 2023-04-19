9e26c3dcd929322ff85dc43014e3d1e441243c10
marwels <mwelfe@gmail.com>
2023-04-19 14:32:33 +0200

    recursive ex pt.2

--- src/index.js
+++ src/index.js
@@ -6,13 +6,34 @@ import "./yapa.css";
 import { sumToLoop } from "./recursiveEx";
 import { sumToRecursion } from "./recursiveEx";
 import { sumToFormula } from "./recursiveEx";
+import { factorial } from "./recursiveEx";
+import { betterFib } from "./recursiveEx";
+import { printListRecursion } from "./recursiveEx";
+import { printListLoop } from "./recursiveEx";
 
 const contentParent = document.getElementById("content");
 
 // sumToLoop(100);
 // console.log(sumToRecursion(100));
-console.log(sumToFormula(100));
+// console.log(sumToFormula(100));
+// console.log(factorial(5));
 
+// console.log(betterFib(77));
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+// printListRecursion(list);
+printListLoop(list);
 // Nav(contentParent, navTree);
 
 // LowerThird(contentParent);
--- src/recursiveEx.js
+++ src/recursiveEx.js
@@ -23,6 +23,86 @@ let sumToFormula = function sumToFormula(n) {
   return sum;
 };
 
+// The task is to write a function factorial(n) that calculates n! using recursive calls.
+// Hint: n! can be written as n * (n-1)! For instance: 3! = 3*2! = 3*2*1! = 6
+
+let factorial = function factorial(n) {
+  if (n === 1) {
+    return n;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+// Write a function fib(n) that returns the n-th Fibonacci number.
+
+let fib = function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+    //       too slow approach
+  }
+};
+
+let betterFib = function betterFib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+};
+
+// Let’s say we have a single - linked list
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+// loop
+let printListLoop = function printListLoop(list) {
+  let tmpList = list;
+  while (tmpList) {
+    console.log(tmpList.value);
+    tmpList = tmpList.next;
+  }
+};
+// recursion
+
+let printListRecursion = function printListRecursion(list) {
+  if (list.next === null) {
+    console.log(list.value);
+    return;
+  } else {
+    console.log(list.value);
+    printListRecursion(list.next);
+  }
+};
+
 export { sumToLoop };
 export { sumToRecursion };
 export { sumToFormula };
+export { factorial };
+export { betterFib };
+export { printListRecursion };
+export { printListLoop };
